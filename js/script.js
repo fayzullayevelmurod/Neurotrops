09681094dfaec1286705776c97642f59e6b88677
SardorQobilov <sardorqobilov555@gmail.com>
2024-04-19 19:41:58 +0500

    question box added

--- js/script.js
+++ js/script.js
@@ -38,12 +38,33 @@ var swiper = new Swiper(".product__img-swiper", {
 });
 
 // products
-const products = document.querySelectorAll(".product__card");
+const products = document.querySelectorAll(".product__img");
 
 products.forEach((product) => {
   const productFavouriteBtn = product.querySelector(".product__favourite");
   productFavouriteBtn.addEventListener("click", function () {
     this.classList.toggle("active");
-    console.log(this);
   });
 });
+
+// products
+// const productFavouriteBtns = document.querySelectorAll(".product__favourite");
+// productFavouriteBtns.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     btn.classList.toggle("active"); // Har bir bosgan tugmachaga "active" klassini qo'shish yoki o'chirish
+//     productFavouriteBtns.forEach(
+//       (item) => item !== btn && item.classList.remove("active")
+//     ); // Boshqa tugmalar uchun "active" klassini o'chirish
+//   });
+// });
+
+// // products.forEach((product) => {
+// // });
+
+// fixed question btn 
+const questionBtn = document.querySelector(".question__box-btn");
+const socialMediaBox = document.querySelector(".socail__media");
+
+questionBtn.addEventListener('click', () => {
+  socialMediaBox.classList.toggle('show')
+})
