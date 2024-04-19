025ef5d696ea0364b4c84780873df6e88a036347
SardorQobilov <sardorqobilov555@gmail.com>
2024-04-19 17:31:13 +0500

    products finished

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// intro swiper
 var swiper = new Swiper(".intro__swiper", {
   slidesPerview: 1,
   navigation: {
@@ -7,6 +8,42 @@ var swiper = new Swiper(".intro__swiper", {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-	},
-	speed: 600,
+  },
+  speed: 600,
+});
+
+// products swiper
+var swiper = new Swiper(".products__swiper", {
+  slidesPerView: 3,
+  grid: {
+    rows: 2,
+  },
+  // slidesPerColumn: 3,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  allowTouchMove: false,
+});
+// product__img-swiper
+var swiper = new Swiper(".product__img-swiper", {
+  slidesPerview: 1,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // effect: "fade",
+  speed: 700,
+});
+
+// products
+const products = document.querySelectorAll(".product__card");
+
+products.forEach((product) => {
+  const productFavouriteBtn = product.querySelector(".product__favourite");
+  productFavouriteBtn.addEventListener("click", function () {
+    this.classList.toggle("active");
+    console.log(this);
+  });
 });
